6c383b0949991e5254b0f5b82cc7d12c49fd5a10
Johan Bäckström <johan@eknaton.com>
2024-06-12 22:20:48 +0200

    Version deployed at korsstygn.online 2024-06-12

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let values = {};
 const checkmark = '&#10006;';
-let mouseIsDown = false;
 
 function markWithX(e) {
   currentValue = (values[e.currentTarget.row] ? values[e.currentTarget.row][e.currentTarget.col] : null) || {} 
@@ -21,50 +20,74 @@ function markWithX(e) {
 function reset() {
   localStorage.clear();
   document.getElementById("color").value = '#000000'
-  setup()
+  setUpEditMode()
 }
 
 function getColor() {
   return document.getElementById("color").value;
 }
 
-function setup() {
+function printFriendly() {
+  window.open('./print.html','_blank','titlebar=no,toolbar=no,menubar=no');
+}
 
-  storedSize = JSON.parse(localStorage.getItem("korsstygnStorlek") || '{}');
-  const columns = document.getElementById("numberOfColumns").value || storedSize?.columns || 60;
-  const rows = document.getElementById("numberOfRows").value || storedSize?.rows || 40;
+function setWidth(columns) {
+  localStorage.setItem("korsstygnBredd", columns)
+  const rows = document.getElementById("numberOfRows")?.value;
+  render(values, rows, columns, true)
+}
+
+function setHeight(rows) {
+  localStorage.setItem("korsstygnHöjd", rows)
+  const columns = document.getElementById("numberOfColumns")?.value;
+  render(values, rows, columns, true)
+}
+
+function setUpEditMode() {
+
+  const columns = localStorage.getItem("korsstygnBredd") || 60;
+  const rows = localStorage.getItem("korsstygnHöjd") || 40;
   document.getElementById("numberOfRows").value = rows;
   document.getElementById("numberOfColumns").value = columns;
-
-  localStorage.setItem("korsstygnStorlek", JSON.stringify({
-    columns,
-    rows
-  }))
-
   try {
     values = JSON.parse(localStorage.getItem("korsstygn")) || {};
   } catch {
     console.error('Unable to read local data, resetting...');
     localStorage.clear();
   }
+  render(values, rows, columns, true);
+}
+
+function setUpPrintMode() {
+  const columns = localStorage.getItem("korsstygnBredd") || 60;
+  const rows = localStorage.getItem("korsstygnHöjd") || 40;
+  try {
+    values = JSON.parse(localStorage.getItem("korsstygn")) || {};
+  } catch {
+    console.error('Unable to read local data');
+  }
+  render(values, rows, columns, false);
+}
+
+function render(input_values, rows, columns, editable) {
   document.getElementById("container").innerHTML = '';
   for (let r = 0; r < rows; r++) {
     let row = document.createElement("div");
     row.style = "display: flex;";
     for (let c = 0; c < columns; c++) {
       let element = document.createElement("div");
-      element.addEventListener("mousedown", markWithX);
-      if (values[r] && values[r][c]?.checked){
-        element.innerHTML = checkmark;
-        element.style.color = values[r][c].color;
+      if (editable) {
+        element.addEventListener("mousedown", markWithX);
       }
-      element.className = "box";
+      if (input_values[r] && input_values[r][c]?.checked){
+        element.innerHTML = checkmark;
+        element.style.color = input_values[r][c].color;
+      }
+      element.className = editable ? "editBox" : "printBox";
       element.col = c
       element.row = r
       row.appendChild(element);
     }
-    document.getElementById("container").appendChild(row); 
+    document.getElementById("container").appendChild(row);
   }
 }
-
-setup();
